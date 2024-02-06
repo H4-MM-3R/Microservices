d2a2e7c0d12e2f19236a466a60c1554bdafed593
Hemram <kuheram777@gmail.com>
2024-02-07 04:00:08 +0530

    Added Product Service

--- ProductService/src/main/java/com/aromablossom/ProductService/DemoApplication.java
+++ ProductService/src/main/java/com/aromablossom/ProductService/DemoApplication.java
@@ -1,11 +1,9 @@
-package com.aromablossom.ServiceRegistry;
+package com.aromablossom.ProductService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ ServiceRegistry/test.md
@@ -0,0 +1,12 @@
+Java Resume
+
+Microservices ---> Both
+
+Full Stack - Salary Database Management Application ---> Both
+
+Kafka - cab booking application ---> impetus
+Kafka - food order application / something ---> swiggy ---> impetus
+
+
+
+
