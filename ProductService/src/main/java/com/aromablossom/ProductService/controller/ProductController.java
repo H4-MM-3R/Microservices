48ba33d39374704e13f2929cfd109dd4f0d4dab8
Hemram <kuheram777@gmail.com>
2024-03-15 11:05:51 +0530

    Added Zipkin and Sleuth for Logs

--- ProductService/src/main/java/com/aromablossom/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/com/aromablossom/ProductService/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,4 +35,8 @@ public class ProductController {
         return new ResponseEntity<>(productResponse, HttpStatus.OK);
     }
     
+    @PutMapping("/reduceQuantity/{id}")
+    public ResponseEntity<Void> reduceQuantity(@PathVariable long productId, long quantity) {
+        return null;
+    }
 }
