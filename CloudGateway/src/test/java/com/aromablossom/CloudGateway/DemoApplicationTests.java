3e289398dbf3371fd934738590cfddfeb176dc72
Hemram <kuheram777@gmail.com>
2024-03-18 20:31:49 +0530

    Added API Gateway

---
+++ CloudGateway/src/test/java/com/aromablossom/CloudGateway/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aromablossom.CloudGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
